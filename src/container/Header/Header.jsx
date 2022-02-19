571b64925b02db791ad303a87a41dc78b86a9b54
Jasir K <62549297+jasirkadarsha721091@users.noreply.github.com>
2022-02-20 01:17:53 +0530

    Navbar and cards completed

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import "./Header.scss";
+import { images } from '../../constants';
+import { motion } from "framer-motion";
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+	return (
+		<div className="header_container">
+			<motion.div
+				className="back_motion_left"
+				initial={{ x: "-100vw" }}
+				animate={{ x: "0vw" }}
+				transition={{ duration: 1, type: "tween" }}
+			></motion.div>
+      			<motion.div
+				className="back_motion_right"
+				initial={{ x: "50vw" }}
+				animate={{ x: "0vw" }}
+				transition={{ duration: 1, type: "tween" }}
+			></motion.div>
+			<motion.div
+				className="place_card"
+				initial={{ y: "-100vh" }}
+				animate={{ y: "0vw" }}
+				transition={{ duration: 2, type: "tween" }}
+			>
+        <div className="pl">Placement</div>
+      </motion.div>
 
-export default Header
+			<motion.div
+				className="intern_card"
+				initial={{ y: "-100vh" }}
+				animate={{ y: "0vw" }}
+				transition={{ duration: 2, type: "tween" }}
+			><div className="pl">Intern</div></motion.div>
+			<motion.div
+				className="project_card"
+				initial={{ y: "+100vh" }}
+				animate={{ y: "0vw" }}
+				transition={{ duration: 2, type: "tween" }}
+			>
+        <div className="pl">Project</div>
+      </motion.div>
+		</div>
+	);
+};
+
+export default Header;
